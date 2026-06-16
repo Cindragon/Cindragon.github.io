7746d7ab4010e1e89eefad51506158d84cd0b034
Cindragon <b7258ter@gmail.com>
2026-06-16 14:38:53 +0800

    change table style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkGfm from 'remark-gfm';
 
 export default defineConfig({
   site: 'https://cindragon.github.io',
   markdown: {
+    remarkPlugins: [remarkGfm],
     shikiConfig: {
       theme: 'github-light',
     },
